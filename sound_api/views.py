822077659e28ffc1105816b239c44e55fc1a3b13
diucap <diucapuig@gmail.com>
2023-10-24 17:07:32 +0200

    made a basic api call

--- sound_api/views.py
+++ sound_api/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from soundback.models import Track
 
 
 # Create your views here.
+@api_view(["GET"])
 def basic(request):
     tracks = Track.objects.all()
     data = []
     for i in tracks:
         data.append({"name" : i.name, "remix" : i.remix, "generes" : i.generes.all()[0].genere, "show": i.show})
-        print(i.serialize())
-    
+        #print(i.serialize())
+        
 
-
-    return JsonResponse({"msg": data})
+    print("asd")
+    return Response(data)
