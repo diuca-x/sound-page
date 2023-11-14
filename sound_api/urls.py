3fab7932bc08397ca39d4f93567bc3d319a5ab5e
diucap <diucapuig@gmail.com>
2023-11-14 19:03:25 +0100

    testing with permisions and authentication

--- sound_api/urls.py
+++ sound_api/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tracks/", views.get_tracks),
-    path("basic/<int:pk>/", views.TrackMixinView.as_view()),
-    path("basic/", views.TrackMixinView.as_view()),
+    path("tracks/", views.TrackListApiView.as_view()),
+    #path("basic/", views.basic),
     path("tracks/<int:pk>/", views.TrackDetailAPIView.as_view()),
     path("tracks/create/", views.TrackCreateAPIView.as_view())
 ]
--- sound_api/views.py
+++ sound_api/views.py
@@ -2,35 +2,13 @@
 import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import generics, mixins
+from rest_framework import generics, permissions
 
 from soundback.models import Track
 from soundback.serializer import TrackSerializer
 
+from .permissions import isStaffEditorPermission
 
-# Create your views here.
-@api_view(["GET"])
-def get_tracks(request):
-    tracks = Track.objects.all()
-    data = []
-    for i in tracks:
-        data.append(TrackSerializer(i).data)
-        #print(i.serialize())
-    print(data)
-
-    
-    return Response(data )
-
-@api_view(["POST"])
-def basic(request, *args,**kwargs):
-    serializer = TrackSerializer(data = request.data)
-    print(serializer.initial_data)
-    if serializer.is_valid(raise_exception=True):
-        #data = serializer.save()
-        print(serializer.data)
-        print("asd")
-        return Response(serializer.data)
-    return Response("asd")
 
 
 class TrackDetailAPIView(generics.RetrieveAPIView):
@@ -41,21 +19,13 @@ class TrackDetailAPIView(generics.RetrieveAPIView):
 class TrackCreateAPIView(generics.CreateAPIView):
     queryset=Track.objects.all()
     serializer_class = TrackSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         print(serializer.validated_data)
         serializer.save()
-    
-class TrackMixinView(mixins.ListModelMixin,mixins.RetrieveModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
-    queryset = Track.objects.all()
+
+class TrackListApiView(generics.ListAPIView):
+    queryset=Track.objects.all()
     serializer_class = TrackSerializer
-    lookup_field="pk"
-    
-    def get(self,request,*args,**kwargs):
-        id = kwargs.get("pk")
-        if id is not None:
-            return self.retrieve(request,*args,**kwargs)
-        return self.list(request, *args, **kwargs)
-    
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
+    permission_classes = [isStaffEditorPermission]  
