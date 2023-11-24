64e237d524f047c5d51d5678e3c3f78b1707fbab
diucap <diucapuig@gmail.com>
2023-11-24 13:42:31 +0100

    started third section, working on the scroll to it from the navbar

--- soundfront/src/components/navbar.jsx
+++ soundfront/src/components/navbar.jsx
@@ -1,42 +1,44 @@
 import React from "react";
 import "../styles/home.css"
 
-const Navbar = () => {
+
+const Navbar = ({toRef}) => {
+    
     return (
         <>
-            <nav class="navbar ">
-                <div class="container-fluid ">
-                    <h1 class="navbar-brand text-light" href="#">BassLineage</h1>
-                    <button class="navbar-toggler  custom-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon  "></span>
+            <nav className="navbar ">
+                <div className="container-fluid ">
+                    <h1 className="navbar-brand text-light" href="#">BassLineage</h1>
+                    <button className="navbar-toggler  custom-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon  "></span>
                     </button>
-                    <div class="offcanvas offcanvas-end  menu_container " tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title title" id="offcanvasDarkNavbarLabel"></h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-end  menu_container " tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title title" id="offcanvasDarkNavbarLabel"></h5>
+                        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" ></button>
                     </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 text-start ps-4">
-                        <li class="nav-item">
-                            <a class="nav-link nav_title" aria-current="page" href="#">Home</a>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 text-start ps-4">
+                        <li className="nav-item">
+                            <p className=" nav_title">Home</p>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">¿Que puedes esperar de estudiar produccion musical el Ableton Live con nosotros?</a>
+                        <li className="nav-item">
+                            <p className="nav-link text-light" href="#" onClick={() => {toRef("expect")}}>¿Que puedes esperar de estudiar produccion musical el Ableton Live con nosotros?</p>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">¿Que ofrecemos adicionalmente?</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="#">¿Que ofrecemos adicionalmente?</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">¿Quien puede estudiar Produccion Musical con nosotros?</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="#">¿Quien puede estudiar Produccion Musical con nosotros?</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">¿Porque Ableton Live?</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="#">¿Porque Ableton Live?</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Ejemplos realizados por alumnos</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="#">Ejemplos realizados por alumnos</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Contacto</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="#">Contacto</a>
                         </li>
                         
                         </ul>
--- soundfront/src/pages/home.jsx
+++ soundfront/src/pages/home.jsx
@@ -2,30 +2,43 @@ import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 import "../styles/home.css";
+import "../styles/expect.css";
+
 import computadora from "../img/computadora.png"
 
 const Home = () =>{
 
     const knowMoreRef = useRef(null)
-    const scrollToKnowMore = () => {
-        if (knowMoreRef.current) {
-          knowMoreRef.current.scrollIntoView({ behavior: 'smooth' });
+    const ExpectRef = useRef(null)
+
+    const toRef = (section) => {
+        let ref
+        switch (section){
+            case "expect":
+                ref = ExpectRef
+                break
+            case 'know_more':
+                ref = knowMoreRef
+                break;
         }
+        
+        if (ref && ref.current) {
+            ref.current.scrollIntoView({ behavior: 'smooth' });
+          }
       };
 
     return(
         <>
-        <div className="home-container  "> 
-            <Navbar/>
+        <div className="home-container"> 
+            <Navbar toRef = {toRef}/>
             <div className=" text-start mt-sm-5 pt-sm-5 ms-sm-5 ps-sm-5  home_content_container">
                 <h1>¡Descubre tu pasion por la Produccion Musical con Ableton Live!</h1>
                 <h3 className="mt-4">Y preparate para firmar con una discografia en tan solo un mes!</h3>
                 <button type="button" className="btn btn-primary rounded-pill contact_btn mt-5">Contactarme</button>  
                 <div className="know_more">
-                    <h4 className="know ">¿Queres saber mas?</h4> 
-                    
+                    <h4 className="know ">¿Queres saber mas?</h4>  
                     <div className="arrow_div  mt-3 p-2">
-                        <i class="fa-solid fa-angle-down fa-2xl arrow" onClick={scrollToKnowMore}></i>   
+                        <i className="fa-solid fa-angle-down fa-2xl arrow" onClick={() => {toRef("know_more")}}></i>   
                     </div>
                     
                 </div>
@@ -45,6 +58,25 @@ const Home = () =>{
                         versatiles a del mundo para la creacion musical?
                     </p>
                 </div>
+        </div>
+        <div className="row expect_container " ref={ExpectRef} >
+            <div className="col  mt-5  expect_col">
+                 <h2 className="expect_title">¿Que puede esperar de estudiar produccion musical en ableton live con nosotros?</h2>   
+            
+            
+                 <p className="expect_text ">
+                    En nuestro curso de produccion musical centrado en Ableton Live, te sumergiras en el emocionante mundo de 
+                    la musica electronica y la produccion contemporanea. Aprenderas desde lo basico hasta tecnicas avanzadas
+                    para crear pistas asombrosas que capturaran la esencia de tu creatividad.
+                 </p>
+                 <p className="expect_text mt-5">
+                    Pero eso no es todo. No nos detenemos en la creacion de las pistas, tambien te enseñaremos las complejidades
+                    del proceso de mezcla y masterizacion, poniendo un enfasis especial en la calidad sonora. Exploraremos el uso
+                    experto de compresores, ecualizadores y exitacion hipocaustica para garantizar que cada nota, cada ritmo
+                    y cada detalle se perciban con una claridad y profundidad excepcionales. 
+                 </p>
+            </div>
+            
         </div>
         
         </>
